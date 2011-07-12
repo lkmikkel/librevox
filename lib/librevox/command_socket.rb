a20bb7ffdcba4ff1bc711cf0deb09d17dfa87e42
Lasse Klok Mikkelsen <lkm@firmafon.dk>
2011-07-12 15:27:48 +0200

    method command is now raising "Connection closed" if it gets disconnected to freeswitch

--- lib/librevox/command_socket.rb
+++ lib/librevox/command_socket.rb
@@ -11,18 +11,33 @@ module Librevox
       @server   = args[:server] || "127.0.0.1"
       @port     = args[:port] || "8021"
       @auth     = args[:auth] || "ClueCon"
+      @timeout  = args[:timeout] || nil
 
-      connect unless args[:connect] == false
+      connect(@timeout) unless args[:connect] == false
     end
 
-    def connect
-      @socket = TCPSocket.open(@server, @port)
-      @socket.print "auth #{@auth}\n\n"
+    def connect(timeout=@timeout)
+      #@socket = TCPSocket.open(@server, @port)
+
+      addr = Socket.getaddrinfo(@server, nil)
+      @socket = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
+
+      if timeout
+        secs = Integer(timeout)
+        usecs = Integer((timeout - secs) * 1_000_000)
+        optval = [secs, usecs].pack("l_2")
+        @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
+        @socket.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
+      end
+      @socket.connect(Socket.pack_sockaddr_in(@port, addr[0][3]))
+
+      @socket.send "auth #{@auth}\n\n"
       read_response
     end
 
     def command *args
-      @socket.print "#{super(*args)}\n\n"
+      check_connection
+      @socket.send "#{super(*args)}\n\n"
       read_response
     end
 
@@ -48,4 +63,16 @@ module Librevox
       headers
     end
   end
+
+  private
+
+  def check_connection
+    begin
+      numread = @socket.recvfrom_nonblock(1)
+      raise "Connection closed??" if numread[0] == ""
+    rescue Errno::EAGAIN => ex
+    end
+  end
+end
+
 end
